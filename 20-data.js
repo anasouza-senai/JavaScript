911a035cafb99c7285ef6195771e72157c197cda
Souza <aninha.clara030807@gmail.com>
2024-03-13 16:05:39 -0300

    atividade string

---
+++ 20-data.js
@@ -0,0 +1,67 @@
+var data1 = new Date();
+console.log(data1);
+
+//Criando um objeto de tipo "data" co data específica Date()
+var data2 = new Date("2007=08-03");
+console.log(data2);
+
+//extreaindo partes de uma data
+var data3 = new Date("2007=08-03");
+console.log(`Data: ${data3}`);
+console.log(`Ano: ${data3.getFullYear()}`);
+console.log(`Mês: ${data3.getMonth() + 1}`);
+console.log(`Dia: ${data3.getDate()}`);
+
+const mes = [
+  "Janeiro",
+  "Feveiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+console.log(`Retornando o mês por escrito!`);
+console.log(`Mês: ${mes[data3.getMonth()]}`);
+
+const diaDaSemana = [
+  "Domingo",
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sábado",
+];
+
+console.log(`Dia da Semana: ${diaDaSemana[data3.getDay()]}`);
+
+console.log("------------------");
+
+const dataNasc = new Date("2007=08-03");
+const dataAtual = new Date();
+
+const diferenca = dataAtual.getTime() - dataNasc.getTime();
+//console.log(`Diferença: ${diferenca}`);
+
+const idade = Math.floor(diferenca / (1000 * 60 * 60 * 24 * 365.25));
+console.log(`Idade: ${idade}`);
+
+console.log("---------------------------");
+
+const diferencaAnos = dataAtual.getFullYear() - dataNasc.getFullYear();
+console.log(`Diferença em anos: ${diferencaAnos}`);
+
+if (
+  dataAtual.getMonth() < dataNasc.getMonth() ||
+  (dataAtual.getMonth() === dataNasc.getMonth() &&
+    dataAtual.getDate() < dataNasc.getDate())
+) {
+  console.log(`Ainda não fez aniversário !`);
+  console.log(`Idade: ${diferencaAnos - 1}`);
+}
--- Atividade8-AnaClaraSouza.js
+++ Atividade8-AnaClaraSouza.js
@@ -1,23 +1,111 @@
-class Aluno {
-  constructor(ra, nome, dataNasc, curso, data) {
-    this.ra = ra;
-    this.nome = nome;
-    this.dataNasc = dataNasc;
-    this.curso = curso;
-    this.data = data;
+// // class Aluno {
+// //   constructor(ra, nome, dataNasc, curso, data) {
+// //     this.ra = ra;
+// //     this.nome = nome;
+// //     this.dataNasc = dataNasc;
+// //     this.curso = curso;
+// //     this.data = data;
+// //   }
+// //   calcularIdade() {
+// //     return 2024 - this.dataNasc;
+// //   }
+// //   apresentar() {
+// //     console.log(`Olá, eu sou ${this.nome}. Estou cursando ${this.curso}`);
+// //   }
+// // }
+
+// // const aluno1 = new Aluno(
+// //   4143,
+// //   "Ana Clara",
+// //   2007,
+// //   "Desenvolvimento de Sistemas"
+// // );
+// // aluno1.apresentar();
+
+// //Crie e utilize uma classe "Sorvete" contendo as propriedades : sabor, preço e tamanho (P | M | G)
+// // Crie um sorvete de morango grande
+// // Crie um sorvete de chocolate pequeno
+// // Crie um sorvete de melancia medio
+// // Altere o preço do sorvete de morango grande para R$ 10,51
+
+// class Sorvete {
+//   constructor(sabor, preco, tamanho) {
+//     this.sabor = sabor;
+//     this.preco = preco;
+//     this.tamanho = tamanho;
+//   }
+//   getPreco() {
+//     return this.preco;
+//   }
+//   setPreco(novoPreco) {
+//     return this.preco;
+//   }
+//   setPreco(novoPreco) {
+//     this.preco = novoPreco;
+//   }
+//   pedido() {
+//     console.log(
+//       `O sorvete escolhido foi ${
+//         this.sabor
+//       }, no valor de R$${this.getPreco()} no tamanho ${this.tamanho}`
+//     );
+//   }
+// }
+
+// const morango = new Sorvete("Morango", 11, "G");
+// const chocolate = new Sorvete("Chocolate", 12, "P");
+// const melancia = new Sorvete("Melância", 2, "M");
+
+// morango, pedido();
+// chocolate.preco();
+// melancia.preco();
+
+// morango.setPreco(10.51);
+// morango.pedido();
+
+console.log("---------------------");
+
+// var texto1 = "A arte é o refúgio dos desesperados!"
+// var texto2 = "a arte é o refúgio dos desesperados!"
+
+class stringComparador {
+  static compareIgnoreCase(texto1, texto2) {
+    return texto1.toLowerCase() === texto2.toLowerCase();
   }
-  calcularIdade() {
-    return 2024 - this.dataNasc;
-  }
-  apresentar() {
-    console.log(`Olá, eu sou ${this.nome}. Estou cursando ${this.curso}`);
+}
+var texto1 = "A arte é o refúgio dos desesperados!";
+var texto2 = "a ARTE é o refúgio dos desesperados!";
+console.log(stringComparador.compareIgnoreCase(texto1, texto2));
+
+console.log("-----------------")
+
+class ExtratorDeNumeros {
+  static extrairNumeros(string) {
+      const regex = /\d+/g;
+      return string.match(regex).map(Number);
   }
 }
 
-const aluno1 = new Aluno(
-  4143,
-  "Ana Clara",
-  2007,
-  "Desenvolvimento de Sistemas"
-);
-aluno1.apresentar();
+// Exemplo de uso:
+const stringExemplo = "Eu tenho 25 anos e meu número de telefone é (14)887566385.";
+const numerosEncontrados = ExtratorDeNumeros.extrairNumeros(stringExemplo);
+console.log(numerosEncontrados); 
+
+console.log("---------------")
+
+class InversorDeFrase {
+  static inverterOrdemDasPalavras(frase) {
+      const palavras = frase.split(" ");
+  
+      const palavrasInvertidas = palavras.reverse();
+      
+      const novaFrase = palavrasInvertidas.join(" ");
+      
+      return novaFrase;
+  }
+}
+
+// Exemplo de uso:
+const fraseOriginal = "Eu sou linda";
+const fraseInvertida = InversorDeFrase.inverterOrdemDasPalavras(fraseOriginal);
+console.log(fraseInvertida);
